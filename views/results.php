415c63c064267a5c9fcc8885184f0d1413ffa1a6
Tinka8 <martina.gatkova@gmail.com>
2024-04-02 18:57:00 +0200

    docs: add comments to switch theme function

--- views/results.php
+++ views/results.php
@@ -2,7 +2,12 @@
 
 <?php 
 
-function switch_theme() {
+/**
+ * Get CSS classes based on query param
+ * 
+ * @return string CSS classes
+ */
+function switch_theme(): string {
     if (isset($_GET['theme']) && $_GET['theme'] === 'dark') {
         return 'bg-zinc-800 text-zinc-100';
     } else {
