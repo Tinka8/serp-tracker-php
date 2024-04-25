8a5e47750a0bd072b2f9266c09dcfbeeadfef36b
Tinka8 <martina.gatkova@gmail.com>
2024-04-25 14:15:37 +0200

    init

---
+++ sites.php
@@ -0,0 +1,10 @@
+<?php require __DIR__ . '/views/partials/header.php'; ?>
+<?php include_once __DIR__ . '/functions.php' ?>
+
+<div class="wrapper bg-zinc-200 py-10">
+    <main class="container max-w-screen-md mx-auto bg-white shadow">
+        123
+    </main>
+</div>
+
+<?php require __DIR__ . '/views/partials/footer.php';
