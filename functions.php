ff2646ec5325ae23b28e4542738253d3858fa2d1
Tinka8 <martina.gatkova@gmail.com>
2024-04-27 21:17:35 +0200

    feat: fix active page function

--- functions.php
+++ functions.php
@@ -23,7 +23,5 @@ function set_active_page($current_page): string {
     if ($_SERVER['REQUEST_URI'] === $current_page) { 
         return 'aria-current="page"' 
         . ' class="border-b border-slate-500 px-2 py-2 text-sm font-medium"'; 
-    } else {
-        return 'class="hover:border-b hover:border-slate-500 px-2 py-2 text-sm font-medium"';
     }
 }
